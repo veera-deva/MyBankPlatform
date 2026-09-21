e9ccbb97371aafea8536c39bf44afd0e91a7b1f6
veera <veera.and@gmail.com>
2026-09-21 18:45:50 +0530

    JWT secret moved to environment variables

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -25,6 +25,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")  
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	annotationProcessor ("org.springframework.boot:spring-boot-configuration-processor")
+
 	
 	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
